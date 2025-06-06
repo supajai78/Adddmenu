30507d44b772f6ffbf23440a1ca1b3770f3d793e
344_10 <344_10@DESKTOP-O0TAAU8>
2025-06-06 16:04:13 +0700

    1 commit

---
+++ src/appmenu/AppMenu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package appmenu;
+
+/**
+ *
+ * @author 344_10
+ */
+public class AppMenu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
